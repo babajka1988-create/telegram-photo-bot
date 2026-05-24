236503a8bde8b76f689d9e8a3fce1e82be6ef3e6
babajka1988-create <babajka1988@gmail.com>
2026-05-24 17:14:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Telegram photo bot started")
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+import asyncio
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Бот работает 🎉")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
